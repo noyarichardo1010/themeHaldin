8733c186d611d26a628e672d150e9a6bd57d60bf
noyarichardo1010 <125939305+noyarichardo1010@users.noreply.github.com>
2026-08-11 13:35:11 +0700

    up

--- template/component/button-whatsapp.php
+++ template/component/button-whatsapp.php
@@ -1,4 +1,4 @@
-<div class="btn btn-whatsapp">
+<div class="btn btn-whatsapp" style="z-index: 999;">
   <a href="https://wa.me/628111545115?text=Hallo.%20Haldin%20Pacific%20Semesta.%20Saya%20ingin%20mengetahui%20informasi%20tentang..%20" target="_blank">
     <i class="fab fa-whatsapp"></i>
   </a>
